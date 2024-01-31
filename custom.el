07a7a5f0c10911f54ab7e2b6421e191b58033ee6
dmorone <diegomorone@protonmail.com>
2024-01-31 22:56:42 +0100

    impatient-mode

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(dockerfile-mode ess php-mode web-mode nyan-mode company powershell flycheck dired-sidebar jenkinsfile-mode git-timemachine git-modes diff-hl git-identity browse-at-remote ssh-agency magit markdown-mode hide-mode-line moody use-package)))
+   '(impatient-mode dockerfile-mode ess php-mode web-mode nyan-mode company powershell flycheck dired-sidebar jenkinsfile-mode git-timemachine git-modes diff-hl git-identity browse-at-remote ssh-agency magit markdown-mode hide-mode-line moody use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

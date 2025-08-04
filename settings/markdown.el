ad28fdf3b89ca02aa9775f659b828191b2c707d8
dmorone <diegomorone@protonmail.com>
2025-08-04 15:48:13 +0200

    fix markdown.el

--- settings/markdown.el
+++ settings/markdown.el
@@ -19,25 +19,4 @@
   :config
   (defun ar/markdown-mode-hook ()
     "Called when entering `markdown-mode'."
-<<<<<<< HEAD
-    (set-fill-column 80)
-    (markdown-toggle-inline-images))
-
-  (defun adviced:markdown-demote (orig-fun &rest r)
-    ;; If not on anything, demote as header.
-    (if (not (or (thing-at-point-looking-at markdown-regex-header-atx)
-                 (thing-at-point-looking-at markdown-regex-header-setext)
-                 (thing-at-point-looking-at markdown-regex-hr)
-                 (markdown-cur-list-item-bounds)
-                 (markdown-table-at-point-p)
-                 (thing-at-point-looking-at markdown-regex-bold)
-                 (thing-at-point-looking-at markdown-regex-italic)))
-        (markdown-insert-header-atx-1)
-      (apply orig-fun r)))
-
-  (advice-add #'markdown-demote
-              :around
-              #'adviced:markdown-demote))
-=======
     (set-fill-column 80)))
->>>>>>> d44787e (docker markdown and behaviour changes)
